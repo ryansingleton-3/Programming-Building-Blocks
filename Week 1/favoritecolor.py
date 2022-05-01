bab28d66eae9ee8654dc9618a7d075aaa48a5f24
ryansingleton-3 <ryanbsingleton3@gmail.com>
2022-04-30 19:18:18 -0500

    first commit

---
+++ Week 1/favoritecolor.py
@@ -0,0 +1,4 @@
+name = input('Hey friend, what is your name? ')
+print('Thanks', name) 
+favoriteColor = input('What is your favorite color? ')
+print(favoriteColor, "is a great choice. It's nice to meet you!")
