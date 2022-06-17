4d081e6e8da497504caa391b409d8586186cfcde
ryansingleton-3 <ryanbsingleton3@gmail.com>
2022-06-16 20:04:51 -0500

    word guessing game added

--- Week 7/word_guessing_game.py
+++ Week 7/word_guessing_game.py
@@ -1,11 +1,24 @@
-print("Welcome to the word guessing game!")
-print()
+print("Welcome to the word guessing game!\n")
 secret_word = "mosiah"
-turn = 1
-guess = input("What is your guess? ")
-while guess.lower() != "mosiah":
-    turn = turn +1
-    print("Your guess was not correct.") 
-    guess = input("What is your guess? ")
-print("Congratulations! You guessed it!")
-print(f'It took you {turn} guesses. ')
+turn = 0
+total_letters = len(secret_word)
+hint = '_ ' * total_letters
+print(f'Your hint is {hint}.')
+guess=None
+while guess != secret_word.lower():
+    guess = input("\nWhat is your guess? ")
+    print("Your hint is: ")
+    turn = turn + 1
+    for i in range(total_letters):
+        letter=guess[i]
+        secret_letter=secret_word[i]
+        if letter==secret_letter:
+            print(letter.upper(), end=' ')
+        elif letter in secret_word:
+            print(letter.lower(),end=' ')
+        else:
+            print("_",end=' ')
+guess.lower()
+print()
+print("Congratulations, you got it!")
+print(f'It took you {turn} guess(es)')
